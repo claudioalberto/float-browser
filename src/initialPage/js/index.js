e83d29cd5ac64f627d3120430d77a0e3254abb8e
Claudio Alberto Faria Gonçalves <contato@claudioalberto.com.br>
2020-05-24 01:40:33 -0300

    Pagina Inicial funcionando com busca por termo e url especifica

--- src/initialPage/js/index.js
+++ src/initialPage/js/index.js
@@ -1,7 +1,24 @@
-import config from '../../config.js'
 
 document.querySelector('#button-search').addEventListener('click', (e) => {
   e.preventDefault()
   let term = document.querySelector('#input-search-term').value
-  console.log(window)
+  let validationUrl = new RegExp("^((http(s?):\/\/(www.)?[a-z]+.com\)|(magnet:\?xt=urn:btih:))")
+  let newUrl
+  // console.log(validationUrl.test(term))
+  if (validationUrl.test(term)) {
+    newUrl = term
+  }
+  else {
+    let termCustom = term.normalize('NFD')
+      .replace(/([\u0300-\u036f]|[^0-9a-zA-Z\s])/g, '')
+      .split(' ')
+    newUrl = 'https://google.com/search?q='
+    termCustom.forEach((element, index) => {
+      newUrl += index != 0 ? '+' + element : element
+    });
+  }
+
+
+  console.log(newUrl)
+  window.location.href = newUrl
 })
